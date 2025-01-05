fbe653f258ab651d2870ac55785f0fa0946f313c
LucjaFekonja <lucija.fekonja@gmail.com>
2025-01-05 14:46:50 +0100

    updated pitch and tempo

--- app/change_emotion.py
+++ app/change_emotion.py
@@ -6,139 +6,139 @@ def adjust(player, pitch, tempo, volume):
 
 # Neutral
 def neutral_to_happy(player):
-    adjust(player, 1, 1.2, 1.2)
+    adjust(player, 1, 1.1, 1.2)
 
 def neutral_to_sad(player):
-    adjust(player, -1, 0.8, 0.8)
+    adjust(player, -0.8, 0.8, 0.8)
   
 def neutral_to_angry(player):
-    adjust(player, 1.5, 1.3, 1.4)
+    adjust(player, -0.2, 1.3, 1.4)
     
 def neutral_to_fearful(player):
-    adjust(player, 0.5, 1.1, 1.1)
+    adjust(player, 0.2, 1.2, 1.1)
     
 def neutral_to_disgusted(player):
-    adjust(player, -0.5, 0.9, 0.85)
+    adjust(player, -1, 0.9, 0.85)
     
 def neutral_to_surprised(player):
-    adjust(player, 2, 1.4, 1.5)
+    adjust(player, 0.8, 1.4, 1.5)
 
 
 # Happy
 def happy_to_neutral(player):
-    adjust(player, -1, 0.8, 0.8)
+    adjust(player, 0, 1, 1.2)
 
 def happy_to_sad(player):
-    adjust(player, -2, 0.6, 0.7)
+    adjust(player, -0.8, 1.1, 1.3)
   
 def happy_to_angry(player):
-    adjust(player, 0.5, 1.1, 1.2)
+    adjust(player, -0.2, 1.3, 1.2)
     
 def happy_to_fearful(player):
-    adjust(player, -0.5, 0.9, 0.9)
+    adjust(player, 0.2, 1.1, 1.2)
     
 def happy_to_disgusted(player):
-    adjust(player, -1.5, 0.8, 0.75)
+    adjust(player, -0.5, 0.8, 1.1)
     
 def happy_to_surprised(player):
-    adjust(player, 1, 1.2, 1.3)
+    adjust(player, 1, 1.4, 1.3)
 
 
 # Sad
 def sad_to_neutral(player):
-    adjust(player, 1, 1.2, 1.2)
+    adjust(player, 0, 1, 1.2)
 
 def sad_to_happy(player):
-    adjust(player, 2, 1.4, 1.3)
+    adjust(player, 0.8, 1.1, 1.3)
   
 def sad_to_angry(player):
-    adjust(player, 1.5, 1.3, 1.4)
+    adjust(player, -0.2, 1.3, 1.4)
     
 def sad_to_fearful(player):
-    adjust(player, 0.5, 1.1, 1.15)
+    adjust(player, 0.2, 1.2, 1.15)
     
 def sad_to_disgusted(player):
-    adjust(player, 0, 1.1, 1.1)
+    adjust(player, -0.5, 1.1, 1.1)
     
 def sad_to_surprised(player):
-    adjust(player, 2.5, 1.5, 1.5)
+    adjust(player, 1.2, 1.5, 1.5)
 
 
 # Angry
 def angry_to_neutral(player):
-    adjust(player, -1.5, 0.7, 0.6)
+    adjust(player, -0.2, 1, 0.6)
 
 def angry_to_happy(player):
-    adjust(player, -0.5, 0.9, 0.8)
+    adjust(player, 0, 1.1, 0.8)
   
 def angry_to_sad(player):
-    adjust(player, -1.5, 0.7, 0.6)
+    adjust(player, -0.2, 1, 0.6)
     
 def angry_to_fearful(player):
-    adjust(player, -0.5, 0.8, 0.9)
+    adjust(player, 0.2, 1.2, 0.9)
     
 def angry_to_disgusted(player):
-    adjust(player, -1, 0.8, 0.85)
+    adjust(player, -0.5, 1.3, 0.85)
     
 def angry_to_surprised(player):
-    adjust(player, 0.5, 1.1, 1.1)
+    adjust(player, 0.5, 1.4, 1.1)
 
 
 # Fearful
 def fearful_to_neutral(player):
-    adjust(player, -0.5, 0.9, 0.9)
+    adjust(player, 0.2, 1, 0.9)
 
 def fearful_to_happy(player):
-    adjust(player, 0.5, 1.2, 1.2)
+    adjust(player, 0.5, 1.1, 1.2)
   
 def fearful_to_sad(player):
-    adjust(player, -1, 0.8, 0.85)
+    adjust(player, -0.8, 1.2, 0.85)
     
 def fearful_to_angry(player):
-    adjust(player, 0.5, 1.2, 1.25)
+    adjust(player, -0.2, 1.3, 1.25)
     
 def fearful_to_disgusted(player):
-    adjust(player, -0.5, 0.9, 0.9)
+    adjust(player, 0.2, 1.2, 0.9)
     
 def fearful_to_surprised(player):
-    adjust(player, 1, 1.3, 1.4)
+    adjust(player, 0.8, 1.4, 1.4)
 
 
 # Disgusted
 def disgusted_to_neutral(player):
-    adjust(player, 0.5, 1.1, 1.15)
+    adjust(player, -1, 1, 1.15)
 
 def disgusted_to_happy(player):
-    adjust(player, 1.5, 1.2, 1.25)
+    adjust(player, 0.8, 1.1, 1.25)
   
 def disgusted_to_sad(player):
     adjust(player, -0.5, 0.9, 0.85)
     
 def disgusted_to_angry(player):
-    adjust(player, 1, 1.3, 1.3)
+    adjust(player, -0.2, 1.3, 1.3)
     
 def disgusted_to_fearful(player):
-    adjust(player, 0.5, 1.1, 1.1)
+    adjust(player, 0.2, 1.2, 1.1)
     
 def disgusted_to_surprised(player):
-    adjust(player, 1.5, 1.4, 1.35)
+    adjust(player, 0.8, 1.4, 1.35)
 
 
 # Surprised
 def surprised_to_neutral(player):
-    adjust(player, -2, 0.6, 0.5)
+    adjust(player, 0.8, 1, 0.5)
 
 def surprised_to_happy(player):
-    adjust(player, -1, 0.8, 0.7)
+    adjust(player, 0.2, 1.1, 0.7)
   
 def surprised_to_sad(player):
-    adjust(player, -2.5, 0.5, 0.5)
+    adjust(player, -0.2, 1.2, 0.5)
     
 def surprised_to_angry(player):
-    adjust(player, -1.5, 0.7, 0.8)
+    adjust(player, -0.5, 1.3, 0.8)
     
 def surprised_to_fearful(player):
-    adjust(player, -1, 0.8, 0.85)
+    adjust(player, -0.2, 1.4, 0.85)
     
 def surprised_to_disgusted(player):
-    adjust(player, -1.5, 0.7, 0.75)
+    adjust(player, -0.5, 1.3, 0.75)
